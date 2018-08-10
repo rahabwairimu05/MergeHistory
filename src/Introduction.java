46bee45fea4b6df11c79775dff505a184cdfb02c
rwangari <rwangari@rwanda.cmu.edu>
2018-08-11 00:14:11 +0200

    Adding all the initial files

---
+++ src/Introduction.java
@@ -0,0 +1,15 @@
+
+public class Introduction {
+
+	public Introduction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//trying it out
+
+}
